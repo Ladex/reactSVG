7b4e7cede9f04553762893885a75eadf2114ecac
Ladex <ladipoilori@yahoo.com>
2015-12-30 00:57:25 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Graph from './components/graph'
 
-export default class App extends Component {
+export default class App extends React.Component {
+  state = { dataSetIndex: 0 }
+
+  selectDataset(event) {
+    this.setState({dataSetIndex: event.target.value});
+  }
+
   render() {
+    let options = this.props.datasets.map((_, index) => {
+      return <option key={index} value={index}>Dataset {index + 1}</option>
+    });
+
     return (
-      <h1>Hello, world.</h1>
-    );
+      <div>
+        <select
+          value={this.state.dataSetIndex}
+          onChange={this.selectDataset.bind(this)} >
+          {options}
+        </select>
+        <Graph data={this.props.datasets[this.state.dataSetIndex]} />
+      </div>
+    )
   }
 }
